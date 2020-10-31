9adf1a97a537ce532f128c73449c1a0ec5a8271e
Kenneth Lau <kelau@oradev.oraclecorp.com>
2020-10-31 16:36:40 -0700

    Refactor common line styles

--- src/beamDiagram.js
+++ src/beamDiagram.js
@@ -1,8 +1,14 @@
 const beamDialog = () => {
-  const boldStyle = {
+  const boldArrowStyle = {
     fontStyle: "bold",
     fontSize: "60px",
     fill: "red",
+  };
+  const redLineForStokeWidth = (width) => {
+    return {
+      stroke: "rgb(255,0,0)",
+      strokeWidth: width,
+    };
   };
 
   return (
@@ -12,16 +18,16 @@ const beamDialog = () => {
         y1="120"
         x2="300"
         y2="120"
-        style={{ stroke: "rgb(255,0,0)", strokeWidth: "2" }}
+        style={redLineForStokeWidth(2)}
       />
       <line
         x1="30"
         y1="140"
         x2="300"
         y2="140"
-        style={{ stroke: "rgb(255,0,0)", strokeWidth: "4" }}
+        style={redLineForStokeWidth(4)}
       />
-      <text x="18" y="183" style={boldStyle}>
+      <text x="18" y="183" style={boldArrowStyle}>
         ↑
       </text>
       <text x="22" y="200" fontSize="large">
@@ -30,7 +36,7 @@ const beamDialog = () => {
       <text x="150" y="200" fontSize="large">
         {window.result.L + "'"}
       </text>
-      <text x="283" y="183" style={boldStyle}>
+      <text x="283" y="183" style={boldArrowStyle}>
         ↑
       </text>
       <text x="286" y="200" fontSize="large">
