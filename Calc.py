9c25daf11e68061b4715c240c0f144f19a0baea9
Kenneth Lau <kenlau>
2020-12-30 01:13:44 -0800

    Abstract input data into Input.py

--- Calc.py
+++ Calc.py
@@ -1,23 +1,16 @@
 # Floor Beam Calculation
 
+# Input
+import Input
+
 # Dependency
 import numpy as np
 import json
 
-# Input
-BeamName = "RB"
-BeamNum = 1
-BeamLength = 15
-loadings = [
-    {'type': 1, 'w': 100},
-    {'type': 2, 'p': 1000, 'l1': 5},
-    {'type': 2, 'p': 1000, 'l1': 5},
-    {'type': 2, 'p': 1000, 'l1': 5},
-    {'type': 2, 'p': 1000, 'l1': 5},
-    {'type': 2, 'p': 1000, 'l1': 5},
-    {'type': 3, 'w': 100},
-    {'type': 4, 'w': 100, 'l1': 2.5, 'l2': 6}
-]
+BeamName = Input.BeamName
+BeamNum = Input.BeamNum
+BeamLength = Input.BeamLength
+loadings = Input.loadings
 
 # Formula definitions
 
