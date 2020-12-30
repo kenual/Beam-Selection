b5a6fa8330d65ea72c39834328eaaaf818415471
Kenneth Lau <kenlau>
2020-12-29 17:05:41 -0800

    Change Flask static content root to /build folder

--- App.py
+++ App.py
@@ -1,15 +1,27 @@
-import os
 from flask import Flask, redirect
+import os
+import logging
 
-app = Flask(__name__)
+# Flask configuration parameters
+static_folder = "build"
+http_port = 8080
+
+static_parent = os.getcwd()
+app = Flask(__name__,
+            static_folder=os.path.join(static_parent, static_folder),
+            static_url_path="/")
 
 
 @app.route('/')
 def index_html():
-    return redirect("static/index.html", code=301)
+    return redirect("index.html", code=301)
 
 
 if __name__ == '__main__':
-    # Bind to PORT if defined, otherwise default to 8000.
-    port = int(os.environ.get('PORT', 8000))
-    app.run(host='0.0.0.0', port=port)
+    logging.basicConfig(level=logging.INFO)
+    logging.info("Path to the root of the application - " + app.root_path)
+    logging.info("Folder with static files - " + app.static_folder)
+    logging.info("Path for the static files on the web - " +
+                 (app.static_url_path if app.static_url_path else "/"))
+
+    app.run(host='0.0.0.0', port=http_port)
